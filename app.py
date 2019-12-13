fe08225f113aa7614b21b3723d2ab5a9d313f64e
Markus Winkler <winkler.info@icloud.com>
2019-12-13 13:58:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ images = [
     "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
     "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-29111-1381845968-0.gif",
     "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "https://www.catgifpage.com/vicious-treadmill"
+    "http://www.catgifpage.com/gifs/284.gif"
 ]
 
 @app.route('/')
